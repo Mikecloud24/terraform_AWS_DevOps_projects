47c68a3ebf0e1f9c8e3477756167780958061e2c
Mikecloud24 <165554071+Mikecloud24@users.noreply.github.com>
2024-09-20 23:47:57 +0200

    Update main.tf
    dynamodb resource block added for terraform lock file.

--- remotebackend_statefile_demo/main.tf
+++ remotebackend_statefile_demo/main.tf
@@ -4,8 +4,19 @@ provider "aws" {
 
 resource "aws_instance" "mikecloud24" {
   instance_type = "t2.micro"
-  ami = "ami-0ebfd941bbafe70c6"
+  ami = "ami-0ebfd941bbafe70c6"    # change this to your specific ami
   tags = {
     name = "demo-instance"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"                  # you can name it depending on your choise.
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
